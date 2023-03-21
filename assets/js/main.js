b4f5100fabccbd2b561e7304254dc986795152e4
glbdmn <gabrieledamiani@hotmail.it>
2023-03-21 14:23:21 +0100

    creato domande in pagina

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
+// esercizio info
+
+// chiedere all'utente il suo nome
+// chedere all'utente il suo cognome
+// chiedere all'utente il suo colore prefe
+// scrivere sulla pagina nomecognomecolorepreferito21
+
+
+
+let nome = prompt ("come ti chiami?")
+let cognome = prompt ("qual'è il tuo cognome")
+let colore = prompt ("qual'è il tuo colore preferito?")
+
 
